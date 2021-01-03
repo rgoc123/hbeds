7d5150a687be68c0570a77da9ca9253d1f56f306
Robert O'Connor <robertgoconnor2@gmail.com>
2021-01-02 19:18:40 -0500

    Add skew and positioning

--- client/src/components/floor.js
+++ client/src/components/floor.js
@@ -3,8 +3,11 @@ import React from 'react'
 import Room from './room'
 
 export default function Floor({ floor }) {
+  const marginTop = (floor.number - 1) * 40
+  const zIndex = floor.number * -1 + 1
+
   return (
-    <div className="floor">
+    <div className="floor" style={{ marginTop, zIndex }}>
       <h3>Floor {floor.number}</h3>
 
       <div className="rooms-cont">
